80839ff83fcc6ac4c2c529b18cec584c7dd0a3f5
kpako3rbp <hansss@list.ru>
2024-05-11 23:30:20 +0400

    starting fix bug with white screen on mobiles

---
+++ shared/ui/PixelizedImg_temp/index.jsx
@@ -0,0 +1,52 @@
+import cl from 'classnames';
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react';
+
+import Pixelit from '@/shared/lib/pixelit';
+import Card from '@/shared/ui/Card';
+
+import styles from './index.module.scss';
+
+const PixelizedImage = (props) => {
+  const { className, src, alt, pixelScale } = props;
+  const imageRef = useRef();
+  const canvasRef = useRef();
+
+  // useEffect(() => {
+  //   if (src && imageRef.current && imageRef.current.complete) {
+  //     pixelizeImg(imageRef.current);
+  //   }
+  // }, []);
+
+  const pixelizeImg = (img) => {
+    const px = new Pixelit({
+      from: img,
+      to: canvasRef.current,
+    });
+    let scale = pixelScale; // Начальный масштаб
+
+    px.setScale(scale).pixelate();
+
+    setInterval(() => {
+      scale = scale === pixelScale ? pixelScale - 1 : pixelScale;
+      px.setScale(scale).pixelate();
+    }, 800);
+  };
+
+  console.log('src', src);
+
+  return (
+    <Card className={cl(styles.image, className)}>
+      <img src={src} alt={alt} />
+    </Card>
+  );
+
+  // return (
+  //   <Card className={cl(styles.image, className)}>
+  //     <img ref={imageRef} src={src} alt={alt} onLoad={() => pixelizeImg(imageRef.current)} />
+  //     <canvas ref={canvasRef}></canvas>
+  //   </Card>
+  // );
+};
+
+export default PixelizedImage;
